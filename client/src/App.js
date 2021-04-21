cbd3187e9655503126fef43723ce818ba455161d
ichbindev <cmlinac@gmail.com>
2021-04-21 18:39:03 -0500

    styled components sample

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import StyleSamples from './components/StyleSamples';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <StyleSamples />
     </div>
   );
 }
