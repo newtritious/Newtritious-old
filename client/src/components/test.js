1e5541385c28ea7018009feb4f978f913a54f18f
Cyber Witch <46911274+AshlynTyler@users.noreply.github.com>
2021-04-21 18:00:32 -0600

    created test component and changed app constructor to class.

---
+++ client/src/components/test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Test extends React.Component{
+    render(){
+        return(
+            <div>
+                Hello World!
+            </div>
+        )
+    }
+}
+
+
+
+export default Test;
