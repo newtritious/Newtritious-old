a85e8eb0a42de65d3113233b77965a3f4b1df7fd
Cyber Witch <46911274+AshlynTyler@users.noreply.github.com>
2021-04-23 20:31:17 -0600

    added barebones css to the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import StyleSamples from './components/StyleSamples';
+import NavBar from './components/NavBar.js'
 import Test from './components/test.js'
+import HomePage from './components/pages/HomePage.js'
+import PageA from './components/pages/PageA.js'
+import PageB from './components/pages/PageB.js'
+import PageC from './components/pages/PageC.js'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 class App extends React.Component {
   render(){
     return (
-      <div className="App">
-        <Test />
-        <StyleSamples />
-      </div>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/page-a">
+            <PageA />
+          </Route>
+          <Route path="/page-b">
+            <PageB />
+          </Route>
+          <Route path="/page-c">
+            <PageC />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }
