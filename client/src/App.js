3337a8a55109d20ec860f8e86dc40afe47c24d12
Ashlyn Tyler <46911274+AshlynTyler@users.noreply.github.com>
2021-04-24 14:37:31 -0600

    Ashlyn branch (#8)
    * initial commit
    
    * commiting so I can merge
    
    * created test component and changed app constructor to class.
    
    * created the basic html and functionality for the nav-bar
    
    * added barebones css to the navbar
    
    * added barebones css to the navbar
    
    * deleted unsused imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import StyleSamples from './components/StyleSamples';
+import NavBar from './components/NavBar.js'
+import HomePage from './components/pages/HomePage.js'
+import PageA from './components/pages/PageA.js'
+import PageB from './components/pages/PageB.js'
+import PageC from './components/pages/PageC.js'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <StyleSamples />
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/page-a">
+            <PageA />
+          </Route>
+          <Route path="/page-b">
+            <PageB />
+          </Route>
+          <Route path="/page-c">
+            <PageC />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledLink = styled(Link)`
+    width: 11%;
+    min-width: 140px;
+    font-size: 40px;
+    text-align: center;
+    padding: 10px;
+
+    &:hover {
+        background: #ddd;
+    }
+`
+
+class NavButton extends React.Component{
+    render(){
+        return(
+            <StyledLink to={this.props.link}>{this.props.name}</StyledLink>
+        )
+    }
+}
+
+class NavBar extends React.Component{
+    render(){
+        return(
+            <div class="flex flex-row h-20">
+                <NavButton name ="Home" link="/" />
+                <NavButton name = "PageA" link ="/page-a"/>
+                <NavButton name = "PageB" link ="/page-b"/>
+                <NavButton name = "PageC" link ="/page-c"/>
+            </div>
+        )
+    }
+}
+
+export default NavBar;
