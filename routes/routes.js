2d99a140b3a2fc4aa3c0dee55ef9df2edc515ff7
Robert Parsons <rmpcoding@gmail.com>
2021-04-27 17:56:50 -0700

    Adds signup route skeletal structure for when User model is created

--- routes/routes.js
+++ routes/routes.js
@@ -2,4 +2,30 @@ module.exports = function(app) {
   app.get("/test", function(req, res) {
     return res.json({test: "success"});
   });
+
+
+  app.post("/signup", async function(req, res) {
+    // Once the Model User Schema is created, we can require it for this to work.
+    // Code will then need to be uncommented out
+
+/* ------------------------ uncomment code below this ----------------------- */
+
+    // const user = new User(req.body);
+    
+    try {
+      // await user.save()
+
+      res.status(201).send({
+        user: "Delete this line and uncomment out line below it once model is in place"
+        // user
+      })
+    } catch(e) {
+      res.status(400).send(e)
+    } 
+  })
+
 };
+
+  
+
+
