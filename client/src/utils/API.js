d6ac870f5c8b01f55dc1a480be1e3a6c16b42d83
Joseph Lynn <josephlynn94@gmail.com>
2021-05-07 14:56:15 -0400

    setup spoonacular API on backend, displaying results on the frontend

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
 const API = {
-  testApi: function() {
-    return axios.get("/test");
-  }
+  testApi: function () {
+    return axios.get('/test');
+  },
+  spoonacularApi: function () {
+    return axios.get('/api/spoonacular');
+  },
 };
 
 export default API;
