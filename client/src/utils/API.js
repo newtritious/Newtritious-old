8beb785f5e57b37ba4c1ee6e7708d5281da2caa3
Robert Parsons <rmpcoding@gmail.com>
2021-06-18 13:59:17 -0700

    Adds login function to utils/API instead of in NavBar and formats API & NavBar.js to prettier format

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,9 +4,23 @@ const API = {
   testApi: function () {
     return axios.get('/test');
   },
+
   spoonacularApi: function () {
     return axios.get('/api/spoonacular');
   },
+
+  login: async function (email, password) {
+    try {
+      const user = await axios.post('/login', {
+        email,
+        password
+      });
+
+      console.log(user);
+    } catch (e) {
+      throw new Error(`Error: ${e}`);
+    }
+  }
 };
 
 export default API;
