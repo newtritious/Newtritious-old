61041974fe658abe879a3fff7f59e2b0b06ddd25
Robert Parsons <55468908+rmpcoding@users.noreply.github.com>
2021-06-24 09:16:03 -0700

    Bcrypt hook user schema (#36)
    * Adds new branch
    
    * Adds working login route and talks to the frontend (no auth yet)
    
    * Adds login function to utils/API instead of in NavBar and formats API & NavBar.js to prettier format
    
    * Adds hashing prehook function in User model for saving hashed passwords in db
    
    * Adds login route password matching functionality
    
    * Adds pw hashing prior to db save, hashed pw comparing in login route, and logic to API login
    
    * Formats selected files
    
    * removes outdated userRoutes.js, which was removed from main codebase

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -11,12 +11,20 @@ const API = {
 
   login: async function (email, password) {
     try {
-      const user = await axios.post('/login', {
-        email,
-        password
-      });
+      const user = await axios
+        .post('/login', {
+          email,
+          password
+        })
+        .catch((error) => {
+          if (error.response.status === 401) alert('Credentials Failed');
+          if (error.response.status === 404) alert('User Not Found');
+        });
+
+      if (!user) return;
 
       console.log(user);
+      return user;
     } catch (e) {
       throw new Error(`Error: ${e}`);
     }
