71048b7eb328dd9045dadb258a51d56af52163a9
Joseph Lynn <josephlynn94@gmail.com>
2021-06-25 05:50:16 -0400

    changed SearchForm to functional component

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -2,30 +2,28 @@ import React from 'react';
 import {
   StyledTextInput,
   StyledSubmit,
-  StyledForm,
+  StyledForm
 } from './styles/StyledInputs';
 
-class SearchForm extends React.Component {
-  render() {
-    return (
-      <div>
-        <StyledForm onSubmit={this.props.onSubmitForm}>
-          <label>Search:</label>
-          <StyledTextInput
-            type='text'
-            name='searchInput'
-            value={this.props.searchInput}
-            onChange={this.props.onInputChange}
-            required
-            maxLengh='48'
-          ></StyledTextInput>
-          <div className='flex flex-row-reverse'>
-            <StyledSubmit value='Go' />
-          </div>
-        </StyledForm>
-      </div>
-    );
-  }
+function SearchForm(props) {
+  return (
+    <div>
+      <StyledForm onSubmit={props.onSubmitForm}>
+        <label>Search:</label>
+        <StyledTextInput
+          type="text"
+          name="searchInput"
+          value={props.searchInput}
+          onChange={props.onInputChange}
+          required
+          maxLength="48"
+        ></StyledTextInput>
+        <div className="flex flex-row-reverse">
+          <StyledSubmit value="Go" />
+        </div>
+      </StyledForm>
+    </div>
+  );
 }
 
 export default SearchForm;
