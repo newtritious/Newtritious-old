5dca83a51f913b8434f7a728bc5a68b1bfa735b8
Ashlyn Tyler <46911274+AshlynTyler@users.noreply.github.com>
2021-07-13 13:56:48 -0600

    Hero (#42)
    * made a very basic hero image to start with
    
    * Added a few more style adjustments to hero and sign up form
    
    * Delete 

---
+++ client/src/components/Hero.js
@@ -0,0 +1,29 @@
+import React from "react";
+import SignUpForm from "./SignUpForm";
+import styled from 'styled-components';
+
+const StyledHero = styled.div`
+    background-image: linear-gradient(#0002,#0002), url("./images/pizza1.jpg");
+    background-size: cover;
+    padding: 50px;
+    padding-bottom: 120px;
+    
+    .signup {
+        width: 30%;
+        margin-left: 60%;
+    }
+`
+
+
+class Hero extends React.Component{
+    render(){
+        return(
+            <StyledHero>
+                <SignUpForm/>
+            </StyledHero>
+        )
+    }
+}
+
+
+export default Hero;
