854c1be3c9d00ec7a356da0c1825dbcd8bec866e
Joseph Lynn <josephlynn94@gmail.com>
2021-08-09 08:08:21 -0400

    reworked redux state, filters and recipes folders are not being used

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,8 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import recipesReducer from './recipes/recipesReducer';
-import filtersReducer from './filters/filtersReducer';
+import searchReducer from './search/searchReducer';
 import userReducer from './user/userReducer';
 
 const middleware = [logger, thunk];
@@ -14,10 +13,8 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(
   combineReducers({
-    // Define a top-level state field named `recipeList`, handled by `recipesReducer`
-    recipeList: recipesReducer,
-    // Define a top-level state field named `filters`, handled by `filtersReducer`
-    filters: filtersReducer,
+    // Define a top-level state field named `search`, handled by `searchReducer`
+    search: searchReducer,
     // Define a top-level state field named `user`, handled by `userReducer`
     user: userReducer
   }),
