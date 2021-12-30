34a390a1796d6b80ddaa8d98a762ec574d442bdf
Robert Parsons <rmpcoding@gmail.com>
2021-12-30 15:15:24 -0800

    saves recipe to db but not user

--- routes/recipes.js
+++ routes/recipes.js
@@ -23,23 +23,45 @@ module.exports = function (app) {
     }
   );
 
+  /* ---------------------------- GET SINGLE RECIPE --------------------------- */
+  // app.get(
+  //   '/recipe/:id',
+  //   passport.authenticate('jwt', { session: false }),
+  //   async (req, res) => {
+  //     const id = Number(req.params.id);
+  //     const _id = req.user._id;
+
+  //     try {
+  //       const user = await User.findOne({
+  //         _id
+  //       });
+
+  //       const savedRecipes = user.savedRecipes;
+
+  //       let recipe = savedRecipes.find((element) => element.id === id);
+
+  //       if (!recipe) return res.status(404).send(`Recipe not found`);
+
+  //       res.status(200).json(recipe);
+  //     } catch (e) {
+  //       res.status(500).send(`Error: ${e}`);
+  //     }
+  //   }
+  // );
+
+  /* ------------------------------ SAVE A RECIPE ----------------------------- */
   app.post(
-    '/recipe/:id',
+    '/recipe',
     passport.authenticate('jwt', { session: false }),
     async function (req, res) {
-      const recipeId = req.params.id;
-      console.log(recipeId);
+      const spoonacularRecipeId = req.body.id;
+
       try {
         const user = await User.findOne({
           _id: req.user._id
         });
 
-        const hasRecipe = await Recipe.find()
-          .where('_id')
-          .in(user.savedRecipes)
-          .equals('61b82c32761c2b372359b937');
-
-        console.log('======= hasRecipe: ', hasRecipe);
+        let hasRecipe = await Recipe.findOne({ id: spoonacularRecipeId });
 
         if (hasRecipe) {
           return res.status(400).json({
@@ -48,51 +70,84 @@ module.exports = function (app) {
           });
         }
 
+        let recipe = await Recipe.create(req.body)
+
+        res.json(recipe);
+
+        // .where('_id')
+        // .in(user.savedRecipes)
+        // .equals('61b82c32761c2b372359b937');
+
+        // const recipe = Recipe.create(req.body);
+
+        // check if recipe exists in Recipe database
+        // if yes
+        // get Recipe ObjectID
+        // save to user's savedRecipes array
+        // else
+        // save to Recipe database
+        // grab Recipe ObjectID
+        // save to user's savedRecipes array
+
+        // const hasRecipe = await Recipe.find()
+        //   .where('_id')
+        //   .in(user.savedRecipes)
+        //   .equals('61b82c32761c2b372359b937');
+
+        // console.log('======= hasRecipe: ', hasRecipe);
+
+        // if (hasRecipe) {
+        //   return res.status(400).json({
+        //     Error:
+        //       'This error occurred because this recipe has been saved already.'
+        //   });
+        // }
+
         // const savedRecipes = user.savedRecipes;
 
         // let recipe = savedRecipes.find((element) => element.id === req.body.id);
 
-        if (!recipe) {
-          const savedRecipe = await Recipe.create(req.body); //returns { <recipe body> }
-          recipe = user.savedRecipes.addToSet(savedRecipe); //returns [ <recipe._id> ]
+        // if (!recipe) {
+        //   const savedRecipe = await Recipe.create(req.body); //returns { <recipe body> }
+        //   recipe = user.savedRecipes.addToSet(savedRecipe); //returns [ <recipe._id> ]
 
-          await user.save();
+        //   await user.save();
 
-          return res.status(201).json(savedRecipe);
-        }
+        //   return res.status(201).json(savedRecipe);
+        // }
 
-        res.status(400).json({
-          Error:
-            'This error occurred because this recipe has been saved already.'
-        });
+        // res.status(400).json({
+        //   Error:
+        //     'This error occurred because this recipe has been saved already.'
+        // });
       } catch (e) {
         res.status(500).json(e);
       }
     }
   );
 
-  app.get(
-    '/recipe/:id',
-    passport.authenticate('jwt', { session: false }),
-    async (req, res) => {
-      const id = Number(req.params.id);
-      const _id = req.user._id;
+  // app.get(
+  //   '/recipe/:id',
+  //   passport.authenticate('jwt', { session: false }),
+  //   async (req, res) => {
+  //     const id = Number(req.params.id);
+  //     const _id = req.user._id;
 
-      try {
-        const user = await User.findOne({
-          _id
-        });
+  //     try {
+  //       const user = await User.findOne({
+  //         _id
+  //       });
 
-        const savedRecipes = user.savedRecipes;
+  //       const savedRecipes = user.savedRecipes;
 
-        let recipe = savedRecipes.find((element) => element.id === id);
+  //       let recipe = savedRecipes.find((element) => element.id === id);
 
-        if (!recipe) return res.status(404).send(`Recipe not found`);
+  //       if (!recipe) return res.status(404).send(`Recipe not found`);
 
-        res.status(200).json(recipe);
-      } catch (e) {
-        res.status(500).send(`Error: ${e}`);
-      }
-    }
-  );
+  //       res.status(200).json(recipe);
+  //     } catch (e) {
+  //       res.status(500).send(`Error: ${e}`);
+  //     }
+  //   }
+  // );
 };
