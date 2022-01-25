9b61f60eb07cd47f40c464ba7ba4c8439beffd85
Amy Moseman <amoseman18@gmail.com>
2022-01-24 19:10:43 -0700

    changed scripts

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const path = require('path')
 const app = express();
 
 // Declare value for PORT
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 // Express middleware
 app.use(cors());
