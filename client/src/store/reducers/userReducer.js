c9076e9f8e41ae871ba72d38ca90dd421ab12ec8
Cyber Witch <46911274+AshlynTyler@users.noreply.github.com>
2022-03-13 15:26:21 -0600

    added saved recipes and relevant selectors and action creators to user reducer in redux store

--- client/src/store/reducers/userReducer.js
+++ client/src/store/reducers/userReducer.js
@@ -1,16 +1,19 @@
 const initialState = {
   username: '',
-  email: ''
+  email: '' ,
+  savedRecipes: []
 };
 
 // action types - help to avoid action name typos
 export const USER_SIGNUP = 'user/userSignup';
 export const USER_LOGIN = 'user/userLogin';
+export const UPDATE_SAVED_RECIPES = 'user/updateSavedRecipes';
 export const USER_LOGOUT = 'user/userLogout';
 
 // selectors - think of selectors as an API for accessing data from Redux state
 export const getUsername = (state) => state.user.username;
 export const getUserEmail = (state) => state.user.email;
+export const getSavedRecipes = (state) => state.user.savedRecipes;
 
 // action creators - function that takes in data and returns a formatted action. Think of them as your API for modifying your data
 export const userSignup = (username) => ({
@@ -22,6 +25,11 @@ export const userLogin = (username) => ({
   type: USER_LOGIN,
   payload: { username }
 });
+
+export const updateSavedRecipes = (savedRecipes) => ({
+  type: UPDATE_SAVED_RECIPES,
+  payload: {savedRecipes}
+})
 
 export const userLogout = () => ({
   type: USER_LOGOUT,
@@ -35,6 +43,9 @@ const userReducer = (state = initialState, action) => {
       return { ...state, ...action.payload };
     }
     case USER_LOGIN: {
+      return { ...state, ...action.payload };
+    }
+    case UPDATE_SAVED_RECIPES: {
       return { ...state, ...action.payload };
     }
     case USER_LOGOUT: {
