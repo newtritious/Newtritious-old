b1c380b47dc8a26db02e22620cc03da62b856fd0
Robert Parsons <rmpcoding@gmail.com>
2022-05-07 16:13:45 -0700

    feat: all routes work

--- models/recipes.js
+++ models/recipes.js
@@ -135,7 +135,6 @@ const recipeSchema = new Schema({
   }
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema)
-
+const Recipe = mongoose.model('Recipe', recipeSchema);
 
 module.exports = Recipe;
